eb01f1bfd78bd1ddea6321f86f45c73829b5451f
bartekkmiecik <barttek.kmiecik9@gmail.com>
2023-03-26 15:58:19 +0200

    Add start screen UI

--- app/src/main/java/com/example/mathemania/MainActivity.kt
+++ app/src/main/java/com/example/mathemania/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.mathemania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
